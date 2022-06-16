073f43eeb6001b2ed1d57cacff3f740b8fe8f05a
Mikhaylov Artem <gits.info20@gmail.com>
2022-06-16 22:05:29 +0300

    prog_001

---
+++ prog_001/Program.cs
@@ -0,0 +1,36 @@
+﻿/* Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+456 -> 3
+78 -> 2
+89126 -> 5 */
+
+using static System.Console;
+Clear();
+WriteLine("Введите число");
+
+WriteLine($"В веденном числе {Getcount(Convert.ToInt32(ReadLine()))} цифры");
+
+
+
+
+
+
+
+
+
+
+
+
+int Getcount(int a)
+{
+    int result = 0;
+    do
+    {
+        a = a/10;
+        result++;
+    
+        
+    }
+    while (a>=1);
+
+    return result;
+}
