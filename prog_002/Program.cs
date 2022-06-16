7cf4adee895e1ba8190d19866564e94ec7162baa
Mikhaylov Artem <gits.info20@gmail.com>
2022-06-16 22:06:01 +0300

    prog_002

---
+++ prog_002/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+4  -> 24
+5  -> 120*/
+
+using static System.Console;
+Clear();
+WriteLine("Введите число");
+
+WriteLine($"Произведение чисел равно {Getn(Convert.ToInt32(ReadLine()))}");
+
+
+
+int Getn(int n)
+{
+    int result=1;
+    for(int i=1; i<=n; i++)
+    {
+        result*=i;
+    }
+    return result;
+}
