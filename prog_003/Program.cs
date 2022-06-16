3fe3d3ee806aee31c5c82820fc0d90ca5e0d1038
Mikhaylov Artem <gits.info20@gmail.com>
2022-06-16 22:03:35 +0300

    Семинар 4

---
+++ prog_003/Program.cs
@@ -0,0 +1,31 @@
+﻿/*Задача 30:**
+Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и 
+единицами в случайном порядке.
+[1,0,1,1,0,1,0,0]
+*/
+
+using static System.Console;
+Clear();
+
+
+WriteLine(FillArray);
+
+
+
+void PrintArray (FillArray)
+{
+    for
+}
+
+int [] FillArray (int [] array)
+{
+    
+    int [] array1 = new int[8];
+    for(int a = 0; a<8; a++)
+    {
+        
+       array1[a]=new Random().Next(0,2);
+
+    }
+    return array1;
+}
